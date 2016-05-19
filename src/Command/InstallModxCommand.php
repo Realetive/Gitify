bdf9f6dfcbe569f0deaa383687e0b563eb799ee7
Roman Ganin <realetive@yandex.ru>
2016-05-19 07:16:00 +0300

    Add DB advanced options

--- src/Command/InstallModxCommand.php
+++ src/Command/InstallModxCommand.php
@@ -98,17 +98,139 @@ class InstallModxCommand extends BaseCommand
         $this->output->writeln("Please complete following details to install MODX. Leave empty to use the [default].");
 
         $helper = $this->getHelper('question');
+        $advanced = false;
+        $dbType = 'mysql';
+        $dbHost = 'localhost'
+        $dbUser = 'root';
+        $dbPass = '';
+        $dbConnectionCharset = 'utf8';
+        $dbCharset = 'utf8';
+        $dbCollation = 'utf8_general_ci';
+        $dbTablePrefix = 'modx_';
 
-        $defaultDbName = basename(GITIFY_WORKING_DIR);
-        $question = new Question("Database Name [{$defaultDbName}]: ", $defaultDbName);
-        $dbName = $helper->ask($this->input, $this->output, $question);
 
-        $question = new Question('Database User [root]: ', 'root');
-        $dbUser = $helper->ask($this->input, $this->output, $question);
+        do {
+            if ($advanced) {
+                $question = new ChoiceQuestion(
+                    'Select MODX database type [mysql]: ',
+                    array('mysql', 'sqlsrv'),
+                    0
+                );
+                $question->setErrorMessage('MODX don\'t support %s database.');
+                $dbType = $helper->ask($this->input, $this->output, $question);
 
-        $question = new Question('Database Password: ');
-        $question->setHidden(true);
-        $dbPass = $helper->ask($this->input, $this->output, $question);
+                $question = new Question("Database host [{$dbHost}]: ", $dbHost);
+                $dbHost = $helper->ask($this->input, $this->output, $question);
+            }
+            $defaultDbName = basename(GITIFY_WORKING_DIR);
+            $question = new Question("Database Name [{$defaultDbName}]: ", $defaultDbName);
+            $dbName = $helper->ask($this->input, $this->output, $question);
+
+            $question = new Question('Database User [root]: ', 'root');
+            $dbUser = $helper->ask($this->input, $this->output, $question);
+
+            $question = new Question('Database Password: ');
+            $question->setHidden(true);
+            $dbPass = $helper->ask($this->input, $this->output, $question);
+
+            if (!$advanced && !mysql_connect($dbHost, $dbUser, $dbPass)) {
+                $this->output->writeln("Cann't connect to localhost as {$dbUser}.");
+                $question = new ConfirmationQuestion('Would you like to activate <info>Advanced Mode Installation</info>? <comment>(Y/n)</comment>', true);
+                if ($helper->ask($input, $output, $question)) {
+                    $advanced = true;
+                }
+            } elseif ($advanced) {
+                $question = new ChoiceQuestion(
+                    'Database connection charset [utf8]: ',
+                    array('armscii8', 'ascii'  , 'big5'  , 'binary' , 'cp1250', 'cp1251'  ,
+                          'cp1256'  , 'cp1257' , 'cp850' , 'cp852'  , 'cp866' , 'cp932'   ,
+                          'dec8'    , 'eucjpms', 'euckr' , 'gb2312' , 'gbk'   , 'geostd8' ,
+                          'greek'   , 'hebrew' , 'hp8'   , 'keybcs2', 'koi8r' , 'koi8u'   ,
+                          'latin1'  , 'latin2' , 'latin5', 'latin7' , 'macce' , 'macroman',
+                          'sjis'    , 'swe7'   , 'tis620', 'ucs2'   , 'ujis'  , 'utf16'   ,
+                          'utf32'   , 'utf8'   , 'utf8mb4'),
+                    'utf8');
+                $question->setErrorMessage('MODX don\'t support %s as connection charset.');
+                $dbConnectionCharset = $helper->ask($this->input, $this->output, $question);
+
+                $question = new Question('Database charset [utf8]: ', 'utf8');
+                $dbCharset = $helper->ask($this->input, $this->output, $question);
+
+                $question = new ChoiceQuestion(
+                    'Database collation [utf8_general_ci]: ',
+                    array('armscii8_bin'            , 'armscii8_general_ci', 'ascii_bin'            ,
+                          'ascii_general_ci'        , 'big5_bin'           , 'big5_chinese_ci'      ,
+                          'binary'                  , 'cp1250_bin'         , 'cp1250_croatian_ci'   ,
+                          'cp1250_czech_cs'         , 'cp1250_general_ci'  , 'cp1250_polish_ci'     ,
+                          'cp1251_bin'              , 'cp1251_bulgarian_ci', 'cp1251_general_ci'    ,
+                          'cp1251_general_cs'       , 'cp1251_ukrainian_ci', 'cp1256_bin'           ,
+                          'cp1256_general_ci'       , 'cp1257_bin'         , 'cp1257_general_ci'    ,
+                          'cp1257_lithuanian_ci'    , 'cp850_bin'          , 'cp850_general_ci'     ,
+                          'cp852_bin'               , 'cp852_general_ci'   , 'cp866_bin'            ,
+                          'cp866_general_ci'        , 'cp932_bin'          , 'cp932_japanese_ci'    ,
+                          'dec8_bin'                , 'dec8_swedish_ci'    , 'eucjpms_bin'          ,
+                          'eucjpms_japanese_ci'     , 'euckr_bin'          , 'euckr_korean_ci'      ,
+                          'gb2312_bin'              , 'gb2312_chinese_ci'  , 'gbk_bin'              ,
+                          'gbk_chinese_ci'          , 'geostd8_bin'        , 'geostd8_general_ci'   ,
+                          'greek_bin'               , 'greek_general_ci'   , 'hebrew_bin'           ,
+                          'hebrew_general_ci'       , 'hp8_bin'            , 'hp8_english_ci'       ,
+                          'keybcs2_bin'             , 'keybcs2_general_ci' , 'koi8r_bin'            ,
+                          'koi8r_general_ci'        , 'koi8u_bin'          , 'koi8u_general_ci'     ,
+                          'latin1_bin'              , 'latin1_danish_ci'   , 'latin1_general_ci'    ,
+                          'latin1_general_cs'       , 'latin1_german1_ci'  , 'latin1_german2_ci'    ,
+                          'latin1_spanish_ci'       , 'latin1_swedish_ci'  , 'latin2_bin'           ,
+                          'latin2_croatian_ci'      , 'latin2_czech_cs'    , 'latin2_general_ci'    ,
+                          'latin2_hungarian_ci'     , 'latin5_bin'         , 'latin5_turkish_ci'    ,
+                          'latin7_bin'              , 'latin7_estonian_cs' , 'latin7_general_ci'    ,
+                          'latin7_general_cs'       , 'macce_bin'          , 'macce_general_ci'     ,
+                          'macroman_bin'            , 'macroman_general_ci', 'sjis_bin'             ,
+                          'sjis_japanese_ci'        , 'swe7_bin'           , 'swe7_swedish_ci'      ,
+                          'tis620_bin'              , 'tis620_thai_ci'     , 'ucs2_bin'             ,
+                          'ucs2_czech_ci'           , 'ucs2_danish_ci'     , 'ucs2_esperanto_ci'    ,
+                          'ucs2_estonian_ci'        , 'ucs2_general50_ci'  , 'ucs2_general_ci'      ,
+                          'ucs2_hungarian_ci'       , 'ucs2_icelandic_ci'  , 'ucs2_latvian_ci'      ,
+                          'ucs2_lithuanian_ci'      , 'ucs2_persian_ci'    , 'ucs2_polish_ci'       ,
+                          'ucs2_roman_ci'           , 'ucs2_romanian_ci'   , 'ucs2_sinhala_ci'      ,
+                          'ucs2_slovak_ci'          , 'ucs2_slovenian_ci'  , 'ucs2_spanish2_ci'     ,
+                          'ucs2_spanish_ci'         , 'ucs2_swedish_ci'    , 'ucs2_turkish_ci'      ,
+                          'ucs2_unicode_ci'         , 'ujis_bin'           , 'ujis_japanese_ci'     ,
+                          'utf16_bin'               , 'utf16_czech_ci'     , 'utf16_danish_ci'      ,
+                          'utf16_esperanto_ci'      , 'utf16_estonian_ci'  , 'utf16_general_ci'     ,
+                          'utf16_hungarian_ci'      , 'utf16_icelandic_ci' , 'utf16_latvian_ci'     ,
+                          'utf16_lithuanian_ci'     , 'utf16_persian_ci'   , 'utf16_polish_ci'      ,
+                          'utf16_roman_ci'          , 'utf16_romanian_ci'  , 'utf16_sinhala_ci'     ,
+                          'utf16_slovak_ci'         , 'utf16_slovenian_ci' , 'utf16_spanish2_ci'    ,
+                          'utf16_spanish_ci'        , 'utf16_swedish_ci'   , 'utf16_turkish_ci'     ,
+                          'utf16_unicode_ci'        , 'utf32_bin'          , 'utf32_czech_ci'       ,
+                          'utf32_danish_ci'         , 'utf32_esperanto_ci' , 'utf32_estonian_ci'    ,
+                          'utf32_general_ci'        , 'utf32_hungarian_ci' , 'utf32_icelandic_ci'   ,
+                          'utf32_latvian_ci'        , 'utf32_lithuanian_ci', 'utf32_persian_ci'     ,
+                          'utf32_polish_ci'         , 'utf32_roman_ci'     , 'utf32_romanian_ci'    ,
+                          'utf32_sinhala_ci'        , 'utf32_slovak_ci'    , 'utf32_slovenian_ci'   ,
+                          'utf32_spanish2_ci'       , 'utf32_spanish_ci'   , 'utf32_swedish_ci'     ,
+                          'utf32_turkish_ci'        , 'utf32_unicode_ci'   , 'utf8_bin'             ,
+                          'utf8_czech_ci'           , 'utf8_danish_ci'     , 'utf8_esperanto_ci'    ,
+                          'utf8_estonian_ci'        , 'utf8_general50_ci'  , 'utf8_general_ci'      ,
+                          'utf8_general_mysql500_ci', 'utf8_hungarian_ci'  , 'utf8_icelandic_ci'    ,
+                          'utf8_latvian_ci'         , 'utf8_lithuanian_ci' , 'utf8_persian_ci'      ,
+                          'utf8_polish_ci'          , 'utf8_roman_ci'      , 'utf8_romanian_ci'     ,
+                          'utf8_sinhala_ci'         , 'utf8_slovak_ci'     , 'utf8_slovenian_ci'    ,
+                          'utf8_spanish2_ci'        , 'utf8_spanish_ci'    , 'utf8_swedish_ci'      ,
+                          'utf8_turkish_ci'         , 'utf8_unicode_ci'    , 'utf8mb4_bin'          ,
+                          'utf8mb4_czech_ci'        , 'utf8mb4_danish_ci'  , 'utf8mb4_esperanto_ci' ,
+                          'utf8mb4_estonian_ci'     , 'utf8mb4_general_ci' , 'utf8mb4_hungarian_ci' ,
+                          'utf8mb4_icelandic_ci'    , 'utf8mb4_latvian_ci' , 'utf8mb4_lithuanian_ci',
+                          'utf8mb4_persian_ci'      , 'utf8mb4_polish_ci'  , 'utf8mb4_roman_ci'     ,
+                          'utf8mb4_romanian_ci'     , 'utf8mb4_sinhala_ci' , 'utf8mb4_slovak_ci'    ,
+                          'utf8mb4_slovenian_ci'    , 'utf8mb4_spanish2_ci', 'utf8mb4_spanish_ci'   ,
+                          'utf8mb4_swedish_ci'      , 'utf8mb4_turkish_ci' , 'utf8mb4_unicode_ci'),
+                    'utf8_general_ci');
+                $dbCollation = $helper->ask($this->input, $this->output, $question);
+
+                $question = new Question('Database connection charset [_modx]: ', '_modx');
+                $dbTablePrefix = $helper->ask($this->input, $this->output, $question);
+            }
+        } while (!mysql_connect($dbHost, $dbUser, $dbPass));
 
         $question = new Question('Hostname [' . gethostname() . ']: ', gethostname());
         $host = $helper->ask($this->input, $this->output, $question);
@@ -149,15 +271,15 @@ class InstallModxCommand extends BaseCommand
         $managerEmail = $helper->ask($this->input, $this->output, $question);
 
         $configXMLContents = "<modx>
-            <database_type>mysql</database_type>
-            <database_server>localhost</database_server>
+            <database_type>{$dbType}</database_type>
+            <database_server>{$dbHost}</database_server>
             <database>{$dbName}</database>
             <database_user>{$dbUser}</database_user>
             <database_password>{$dbPass}</database_password>
-            <database_connection_charset>utf8</database_connection_charset>
-            <database_charset>utf8</database_charset>
-            <database_collation>utf8_general_ci</database_collation>
-            <table_prefix>modx_</table_prefix>
+            <database_connection_charset>{$dbConnectionCharset}</database_connection_charset>
+            <database_charset>{$dbCharset}</database_charset>
+            <database_collation>{$dbCollation}</database_collation>
+            <table_prefix>{$dbTablePrefix}</table_prefix>
             <https_port>443</https_port>
             <http_host>{$host}</http_host>
             <cache_disabled>0</cache_disabled>
